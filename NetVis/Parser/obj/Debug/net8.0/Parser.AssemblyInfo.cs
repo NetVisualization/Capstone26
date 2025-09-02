570810f09fc681a5b2f95c42a080bd62a863223d
Tomis Hamilton <tomisnhamilton@gmail.com>
2025-09-02 11:19:20 -0600

    impl: parsing
    curr issue: runtime columns not init

--- NetVis/Parser/obj/Debug/net8.0/Parser.AssemblyInfo.cs
+++ NetVis/Parser/obj/Debug/net8.0/Parser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbacd7f98258dd0047248083a0a3e20779f2fec2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ebb1081bb344a07e287f6ca7bc27817a632329")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
