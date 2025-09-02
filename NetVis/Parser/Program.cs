a6f4e2d2f98a5e1d8f3d39cb3d786a1003204ea4
Tomis Hamilton <tomisnhamilton@gmail.com>
2025-09-02 09:55:33 -0600

    DB Connection test

---
+++ NetVis/Parser/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClickHouse.Driver.ADO;
+using ClickHouse.Driver.Copy;
+using System.Data;
+using ClickHouse.Driver.Utility;
+
+
+var connectionString = "Host=100.111.112.111;Protocol=http;Database=net;Username=capstone;Password=boogle";
+
+using (var connection = new ClickHouseConnection(connectionString))
+{
+    connection.Open();
+    Console.WriteLine("Connected");
+    
+    using (var command = connection.CreateCommand())
+    {
+        command.AddParameter("id", "Int64", 10);
+        command.CommandText = "USE net;";
+        command.ExecuteNonQuery();
+        command.CommandText = "SHOW TABLES;";
+        var rowsAffected = command.ExecuteNonQuery();
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine(reader.GetString(0));
+        }
+    }
+}
