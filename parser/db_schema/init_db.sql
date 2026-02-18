977addb2b974ed70e93c8a525fb471fbea85dc86
Tomis Hamilton <tomisnhamilton@gmail.com>
2026-02-18 09:38:47 -0700

    UPD: Add zeek.sql to docker init

--- parser/db_schema/init_db.sql
+++ parser/db_schema/init_db.sql
@@ -261,3 +261,42 @@ SELECT
     packet_id,
     hex(bytes) AS bytes_hex
 FROM net.raw_bytes;
+
+-- zeek.sql
+DROP TABLE IF EXISTS net.weird;
+
+CREATE TABLE net.weird
+(
+    ts DateTime64(6, 'UTC'),
+
+    uid Nullable(String),
+
+    orig_h Nullable(IPv6),
+    orig_p Nullable(UInt16),
+    resp_h Nullable(IPv6),
+    resp_p Nullable(UInt16),
+
+    name LowCardinality(String),
+    addl Nullable(String),
+
+    notice UInt8,
+    peer LowCardinality(String),
+
+    source LowCardinality(Nullable(String)),
+    identifier Nullable(String),
+
+    day Date MATERIALIZED toDate(ts)
+)
+    ENGINE = MergeTree
+        PARTITION BY day
+        ORDER BY
+            (
+             day,
+             ts,
+             name,
+             ifNull(uid, ''),
+             ifNull(orig_h, toIPv6('::')),
+             ifNull(orig_p, 0),
+             ifNull(resp_h, toIPv6('::')),
+             ifNull(resp_p, 0)
+                );
