76e31276ffdd08e3836bc8eda25c2262471890b8
Emile Olivier <eolivier8168@gmail.com>
2026-02-20 17:20:54 +0000

    install skel script based on arch changes we discussed

---
+++ parser/scripts/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ensure sudo
+
+# prompt for credentials and save to env file
+
+# install dependencies (docker, docker-compose, rust, cargo, etc.)
+
+# start containers
+
+    # validate interface for zeek
+
+# direct user to run the parsers via the parser.sh script
